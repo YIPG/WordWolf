dcd615b895f5b79fe73cdfadcf84db8a9f1b62b3
yipg <around.mr.child@gmail.com>
2020-01-20 01:58:32 +0900

    reduce css size

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,15 @@
-const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.tsx", "./src/**/*.ts", "./public/index.html"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 };
