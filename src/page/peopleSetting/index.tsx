88145404bef89379389e5589c869d83bfa9970d4
yipg <around.mr.child@gmail.com>
2020-01-20 02:04:57 +0900

    changed style a bit

--- src/page/peopleSetting/index.tsx
+++ src/page/peopleSetting/index.tsx
@@ -31,49 +31,44 @@ const PeopleSetting: React.FC = () => {
 
   const num = nameList.length;
   return (
-    <div className="h-full">
-      <div className="flex flex-col items-center">
-        <span className="text-4xl text-green-400 font-bold pt-24">
-          プレイヤー設定
+    <div className="flex flex-col items-center mb-16">
+      <span className="text-4xl text-green-400 font-bold pt-24">
+        プレイヤー設定
+      </span>
+      <span className="text-lg font-semibold pt-6 pb-4">人数</span>
+      <div className="flex flex-row justify-center items-center pb-8">
+        <span onClick={() => num > 3 && decrement()} className="text-5xl px-6">
+          <FontAwesomeIcon icon={faCaretLeft} />
         </span>
-        <span className="text-lg font-semibold pt-6 pb-4">人数</span>
-        <div className="flex flex-row justify-center items-center pb-8">
-          <span
-            onClick={() => num > 3 && decrement()}
-            className="text-5xl px-6"
-          >
-            <FontAwesomeIcon icon={faCaretLeft} />
-          </span>
-          <span className="text-3xl font-bold px-16 my-auto">{num}</span>
-          <span onClick={() => increment()} className="text-5xl px-6">
-            <FontAwesomeIcon icon={faCaretRight} />
-          </span>
-        </div>
-        <form
-          className="flex flex-col items-center"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div>
-            {nameList.map((name, index) => {
-              return (
-                <NameItem
-                  key={index}
-                  register={register}
-                  index={index}
-                  name={name}
-                  // onDeleteEl={onDeleteEl}
-                />
-              );
-            })}
-          </div>
-          <span className="py-4 text-2xl" onClick={increment}>
-            <FontAwesomeIcon icon={faPlusCircle} />
-          </span>
-          <button type="submit" className="btn btn-green">
-            ゲーム設定へ
-          </button>
-        </form>
+        <span className="text-3xl font-bold px-16 my-auto">{num}</span>
+        <span onClick={() => increment()} className="text-5xl px-6">
+          <FontAwesomeIcon icon={faCaretRight} />
+        </span>
       </div>
+      <form
+        className="flex flex-col items-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div>
+          {nameList.map((name, index) => {
+            return (
+              <NameItem
+                key={index}
+                register={register}
+                index={index}
+                name={name}
+                // onDeleteEl={onDeleteEl}
+              />
+            );
+          })}
+        </div>
+        <span className="py-4 text-2xl" onClick={increment}>
+          <FontAwesomeIcon icon={faPlusCircle} />
+        </span>
+        <button type="submit" className="btn btn-green">
+          ゲーム設定へ
+        </button>
+      </form>
     </div>
   );
 };
