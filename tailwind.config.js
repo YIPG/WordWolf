386dbe79e6509cfdc2be86b92cfee77f5626b5e3
yipg <around.mr.child@gmail.com>
2020-01-30 02:52:44 +0900

    add tailwind plugin transition

--- tailwind.config.js
+++ tailwind.config.js
@@ -425,6 +425,54 @@ module.exports = {
       "30": "30",
       "40": "40",
       "50": "50"
+    },
+    transitionProperty: {
+      // defaults to these values
+      none: "none",
+      all: "all",
+      color: "color",
+      bg: "background-color",
+      border: "border-color",
+      colors: ["color", "background-color", "border-color"],
+      opacity: "opacity",
+      transform: "transform"
+    },
+    transitionDuration: {
+      // defaults to these values
+      default: "250ms",
+      "0": "0ms",
+      "100": "100ms",
+      "250": "250ms",
+      "500": "500ms",
+      "750": "750ms",
+      "1000": "1000ms"
+    },
+    transitionTimingFunction: {
+      // defaults to these values
+      default: "ease",
+      linear: "linear",
+      ease: "ease",
+      "ease-in": "ease-in",
+      "ease-out": "ease-out",
+      "ease-in-out": "ease-in-out"
+    },
+    transitionDelay: {
+      // defaults to these values
+      default: "0ms",
+      "0": "0ms",
+      "100": "100ms",
+      "250": "250ms",
+      "500": "500ms",
+      "750": "750ms",
+      "1000": "1000ms"
+    },
+    willChange: {
+      // defaults to these values
+      auto: "auto",
+      scroll: "scroll-position",
+      contents: "contents",
+      opacity: "opacity",
+      transform: "transform"
     }
   },
   variants: {
@@ -493,8 +541,13 @@ module.exports = {
     whitespace: ["responsive"],
     width: ["responsive"],
     wordBreak: ["responsive"],
-    zIndex: ["responsive"]
+    zIndex: ["responsive"],
+    transitionProperty: ["responsive"],
+    transitionDuration: ["responsive"],
+    transitionTimingFunction: ["responsive"],
+    transitionDelay: ["responsive"],
+    willChange: ["responsive"]
   },
   corePlugins: {},
-  plugins: []
+  plugins: [require("tailwindcss-transitions")()]
 };
